aa475cd4c130c161e4590af92944e283c74eb6e4
gabo <josegabrielruizperez44@gmail.com>
2025-06-16 11:15:59 -0400

    creacion id y hora

--- funtions.py
+++ funtions.py
@@ -1,17 +1,36 @@
 import json
 import os
+from datetime import datetime
 
 print('indique la tarea a realizar')
 task = input('')
 print('indique el estado de la tarea')
-print('0 = done, 1 = in progress, 2 = todo')
+print('0 = hecho, 1 = en progreso, 2 = pendiente')
 progress_task = int(input())
 
 class Task:
     def __init__ (self,task):
         self.task = task
-        self.progress_task = ['done', 'in progress','todo'] 
+        self.progress_task = ['hecho', 'en progreso','pendiente']
+        self.id = 0
+        self.list_task = {}
 
+    def time_task(self):
+        time = str(datetime.now())
+        print(time)
+        return time
+    
+    def id_task(self):
+        if os.path.exists('task.json'):
+            list_task = read_task_json()
+            finale_task = list_task.popitem()
+            number_task = int(finale_task[0])
+            self.id = number_task + 1
+            return self.id
+        else:
+            self.id += 1
+            return self.id
+            
     def mark_progress(self,progress):
         if progress < len(self.progress_task):
             task_progress = self.progress_task[progress]
@@ -20,31 +39,38 @@ class Task:
               
 
     def to_dict(self, progress):
-        list_task = {self.task : self.progress_task[progress]}
-        return list_task
+        id_task = self.id_task()
+        time_task = self.time_task()
+        print(id_task)
+        self.list_task = {id_task : {'tarea' : self.task , 'progreso' : self.progress_task[progress] , 'hora' : time_task}}
+        print(self.list_task)
+        return self.list_task
 
     def __str__(self):
         return f'{self.list_task} Añadido'
-        
-def read_task_json():
-    with(open('task.json','r') as file):
-        task = json.load(file)
-        return task
-    
-            
-def save_task_json(task):
-    with(open('task.json','w') as file):
-        json.dump(task,file,indent=4)
 
-def save(list_task):
-    if os.path.exists('task.json'):
-        task = read_task_json()
-        print(type(task))
-        task = task | list_task
-        print(task)
-        save_task_json(task)
-    else:
-        save_task_json(list_task)
+
+class save_task:
+    def __init__(self,task):
+        self.task = task
+        self.id = 0
+
+    def read_task_json(self):
+        with(open('task.json','r') as file):
+            self.task = json.load(file)
+            return task
+        
+    def save_task_json(self,task):
+        with(open('task.json','w') as file):
+            json.dump(task,file,indent=4)
+
+    def save(self, list_task):
+        if os.path.exists('task.json'):
+            task = read_task_json()
+            task = task | list_task
+            self.save_task_json(task)
+        else:
+            self.save_task_json(list_task)
 
 class eliminate_task:
     def __init__(self,task):
@@ -63,14 +89,16 @@ class eliminate_task:
 
 
 
-prueba = eliminate_task(task)
-prueba.eliminate(task)
+#prueba = eliminate_task(task)
+#prueba.eliminate(task)
 
 
 
 
 
-#list_task = Task(task)
-#list_task.mark_progress(progress_task)
+list_task = Task(task)
+list_task.mark_progress(progress_task)
+print(list_task)
 
-#save(list_task.to_dict(progress_task))
+guardado = save_task(list_task)
+guardado.save(list_task.to_dict(progress_task))
